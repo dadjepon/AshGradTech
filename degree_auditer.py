9c2c8bd17ca505554a916daeb362ff64ea6a5afa
Aaron Tsatsu Tamakloe <aarontmkl@gmail.com>
2023-12-10 20:11:38 +0000

    degree audit program

--- degree_auditer.py
+++ degree_auditer.py
@@ -1,14 +1,16 @@
+import json
 from process_text import parse_transcript
 from read_text import process_document_sample as rd
 from course_reqs import *
 
 class Student:
 
-    def __init__(self, transcript, year, major, year_group):
+    def __init__(self, transcript, level, major, year_group, semester):
         self.transcript = transcript 
-        self.year = year
+        self.level = level
         self.major = major
         self.year_group = year_group
+        self.semester = semester
 
     def evaluate_transcript(self):
         # Calculate credits from transcript
@@ -18,19 +20,18 @@ class Student:
         # num_semesters = sum(1 for semester in transcript.keys() if re.match(r'^Semester ?\d', semester))
         
         for semester in self.transcript:
-            for course_code in semester:
+            for course_code in self.transcript[semester]:
                 grade = self.transcript[semester][course_code]["grade"]
                 
                 if grade in ["A", "A+", "B", "B+", "C", "C+", "D+"]:
-                    credits = self.transcript[semester][course_code]["credits"]  
+                    credits = float(self.transcript[semester][course_code]["credits"])
                     total_credits += credits
                 
                 elif grade in ["D", "E"]:
                     failed_courses.append(self.transcript[semester][course_code]["course_title"])
         
-        requirements = self.major.get_requirements(self.year_group)
-
-        if credits >= requirements[self.year]:
+        requirements = self.major.get_requirements(self.year_group, self.level, self.semester)
+        if total_credits >= requirements:
             status = "On Track"
         else: 
             status = "Not on track"
@@ -54,14 +55,16 @@ if __name__ == "__main__":
         field_mask = "text, layout",
         processor_version_id = "ad8664f46cdd7d84"
     )
-    transcript = parse_transcript()
+    transcript = json.loads(parse_transcript())
+    # print(transcript)
 
-    major = CS
-    year = 2
-    year_group = 2024
+    major = CS()
+    level = 3 #current level
+    semester = 1 #current semester
+    year_group = 2024 #year of graduation
 
-    student = Student(transcript, year, major, year_group)
-    reqs = student.major.get_requirements(year_group)
+    student = Student(transcript, level, major, year_group, semester)
+    # reqs = student.major.get_requirements(year_group=year_group, level=level, semester=semester)
     credits = student.evaluate_transcript()
 
     print(
