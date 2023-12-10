9279570d4f59a855db9aab0dc6ac90374fa2a639
Aaron Tsatsu Tamakloe <aarontmkl@gmail.com>
2023-12-10 02:27:36 +0000

    ocr processing with custom ai

--- read_text.py
+++ read_text.py
@@ -1,49 +1,66 @@
-#pylint: disable = no-name-in-module, missing-module-docstring, line-too-long, trailing-whitespace
-import google.auth 
-from google.cloud import documentai
+from typing import Optional
+import google.auth
+from google.api_core.client_options import ClientOptions
+from google.cloud import documentai  # type: ignore
 
-def online_process(file_path: str) -> documentai.Document:
-    '''
-    Args:
-        project_id (str) : The Google Cloud Project
 
-    Return:
-        text (response): Extracted Text
-    '''
-    processor_id, project_id, location = "7e6b9cb13e2f136a", 866827155501, "us"
-    opts = {"api_endpoint": f"{location}-documentai.googleapis.com"}
+def process_document_sample(
+    project_id: str,
+    location: str,
+    processor_id: str,
+    file_path: str,
+    mime_type: str,
+    field_mask: Optional[str] = None,
+    processor_version_id: Optional[str] = None,
+) -> None:
+    # You must set the `api_endpoint` if you use a location other than "us".
+    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
 
     credential_path = "ashgradcheck-e11866cd0200.json"
 
     credentials, _ = google.auth.load_credentials_from_file(
         scopes=["https://www.googleapis.com/auth/cloud-platform"], filename=credential_path, quota_project_id=project_id
     )
-    # Instantiates a client
-    documentai_client = documentai.DocumentProcessorServiceClient.from_service_account_file(
-        client_options=opts, credentials=credentials, filename=credential_path)
 
-    resource_name = documentai_client.processor_path(
-        project_id, location, processor_id)
+    client = documentai.DocumentProcessorServiceClient(client_options=opts, credentials=credentials)
+
+    if processor_version_id:
+        name = client.processor_version_path(
+            project_id, location, processor_id, processor_version_id
+        )
+    else:
+        # The full resource name of the processor, e.g.:
+        # `projects/{project_id}/locations/{location}/processors/{processor_id}`
+        name = client.processor_path(project_id, location, processor_id)
 
     # Read the file into memory
-    with open(file_path, "rb") as file:
-        file_content = file.read()
+    with open(file_path, "rb") as image:
+        image_content = image.read()
 
-    # Load Binary Data into Document AI RawDocument Object
-    raw_document = documentai.RawDocument(
-        content=file_content, mime_type="application/pdf")
+    # Load binary data
+    raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
+
 
     # Configure the process request
     request = documentai.ProcessRequest(
-        name=resource_name, raw_document=raw_document)
+        name=name,
+        raw_document=raw_document,
+        field_mask=field_mask,
+        # process_options=process_options,
+    )
 
-    # Use the Document AI client to process the sample form
-    response = documentai_client.process_document(request=request)
+    result = client.process_document(request=request)
 
-    return response.document.text
+    with open("extracted.txt", "w", encoding="utf-8") as output_file:
+        output_file.write(result.document.text)
 
-
-# program driver
 if __name__ == "__main__":
-    FILE_PATH = "jakman_solutions_milestone_3.pdf"
-    print(online_process(file_path=FILE_PATH))
+    process_document_sample(
+        project_id = "866827155501",
+        location = "us",
+        processor_id = "61abf4ec482095a0",
+        file_path = "trans.pdf",
+        mime_type = "application/pdf",
+        field_mask = "text, layout",
+        processor_version_id = "ad8664f46cdd7d84"
+    )
